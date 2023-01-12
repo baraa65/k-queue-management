18a3490b189450c9dd75c79e7e28b0af54898727
baraa65 <baraash65@hotmail.com>
2023-01-12 21:21:20 +0200

    initial commit

---
+++ windows/KQueueManagement/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::KQueueManagement::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::KQueueManagement::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
